5fb8c37564d72a729ae596f36048453a4ab36da9
midhuns-aot <midhun.s@aot-technologies.com>
2022-11-13 23:44:54 +0530

    fourth commit

--- lms/src/Components/Loginpage.js
+++ lms/src/Components/Loginpage.js
@@ -8,19 +8,19 @@ function Loginpage() {
         <h1>LMS</h1>
         <div className="center-main-division">
           <h2>Login</h2>
-          <p>Welcome back! Please enter your details.</p>
+          <p className="welcome">Welcome back! Please enter your details.</p>
           <div className="link-div">
-            <a href="adm">Admin</a> <a href="stu">Student</a>
+          <p>  <a href="adm">Admin</a> <a className="student" href="std">Student</a> </p>
           </div>
 
           <form>
-            <div>
-            <label htmlFor="Email">Email</label>
+            <p htmlFor="Email">Email
             <br />
-            <input type="text" placeholder="Enter your email" /> <br /></div>
-            <div>
-            <label htmlFor="Password">Password</label> <br />
-            <input type="password" placeholder="Enter your password" /> <br /></div>
+            <input type="text" placeholder="Enter your email" /> <br />
+            </p>
+            <p>Password<br />
+            <input type="password" placeholder="Enter your password" /> <br />
+            </p>
             <button type="submit">Login</button>
           </form>
         </div>
